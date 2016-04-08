1d1df12402754e109f3b9e66b63d4a7ba46405e6
Soatok <soatok@yahoo.com>
2016-04-08 15:26:19 -0400

    Create left-pad.php

---
+++ left-pad.php
@@ -0,0 +1,15 @@
+<?php
+if (!function_exists('left_pad')) {
+    /**
+     * Leftp-pad a string
+     * 
+     * @param string $str
+     * @param int $length
+     * @param string $space
+     * @return string
+     */
+    function left_pad($str, $length = 0, $space = ' ')
+    {
+        return str_pad($str, $length, $space, STR_PAD_LEFT);
+    }
+}
