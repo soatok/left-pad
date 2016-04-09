4a50559f2486e21c892b7406faa19cded2ec4db4
Soatok <soatok@yahoo.com>
2016-04-08 21:28:05 -0400

    Add multibyte support.

--- left-pad.php
+++ left-pad.php
@@ -6,10 +6,17 @@ if (!function_exists('left_pad')) {
      * @param string $str
      * @param int $length
      * @param string $space
+     * @param string $encoding
      * @return string
      */
-    function left_pad($str, $length = 0, $space = ' ')
+    function left_pad($str, $length = 0, $space = ' ', $encoding = '8bit')
     {
+        if (function_exists('mb_strlen')) {
+            while (mb_strlen($str, $encoding) < $length) {
+                $str = $space . $str;
+            }
+            return $str;
+        }
         return str_pad($str, $length, $space, STR_PAD_LEFT);
     }
 }
